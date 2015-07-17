f73afd72a1ca914dd8e11519082fe282a5034f7c
alonso.torres <alonso.torres@kaleidos.net>
2015-07-17 12:12:35 +0200

    PIWEEK

--- js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [uxbox.core] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "dev", :on-jsload (fn [& x] (if js/uxbox.core.on-js-reload (apply js/uxbox.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'uxbox.core/on-js-reload' is missing"))), :websocket-url "ws://localhost:3449/figwheel-ws"})
-
--- js/compiled/out/uxbox/time.cljs
+++
@@ -1,5 +0,0 @@
-(ns uxbox.time
-  (:require cljsjs.moment))
-
-(defn ago [time]
-  (.fromNow (js/moment time)))
